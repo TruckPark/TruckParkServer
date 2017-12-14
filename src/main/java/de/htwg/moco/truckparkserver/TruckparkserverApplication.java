ea24ce39d52522ed592ac097e4720f3a680426e6
Sebastian.Thuemmel <sethuemm@htwg-konstanz.de>
2017-12-14 19:03:13 +0100

    initial setup spring boot

---
+++ src/main/java/de/htwg/moco/truckparkserver/TruckparkserverApplication.java
@@ -0,0 +1,13 @@
+package de.htwg.moco.truckparkserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckparkserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckparkserverApplication.class, args);
+	}
+}
---
+++ src/test/java/de/htwg/moco/truckparkserver/TruckparkserverApplicationTests.java
@@ -0,0 +1,16 @@
+package de.htwg.moco.truckparkserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TruckparkserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
