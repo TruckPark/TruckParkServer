08255015f7e43ee5b0a34caa303b206022a74491
Sebastian Thümmel <sethuemm@htwg-konstanz.de>
2017-12-31 15:44:31 +0100

    implemented post interface to receive list of latlong coordinates as path

--- src/main/java/de/htwg/moco/truckparkserver/TruckparkserverApplication.java
+++ src/main/java/de/htwg/moco/truckparkserver/TruckparkserverApplication.java
@@ -1,8 +1,10 @@
 package de.htwg.moco.truckparkserver;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TruckparkserverApplication {
@@ -10,4 +12,9 @@ public class TruckparkserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TruckparkserverApplication.class, args);
 	}
+
+	@Bean
+    public ObjectMapper objectMapper(){
+	    return new ObjectMapper();
+    }
 }
--- src/main/java/de/htwg/moco/truckparkserver/controller/Controller.java
+++ src/main/java/de/htwg/moco/truckparkserver/controller/Controller.java
@@ -1,13 +1,24 @@
 package de.htwg.moco.truckparkserver.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.Http;
+import com.google.maps.model.LatLng;
 import de.htwg.moco.truckparkserver.model.ParkingLot;
 import de.htwg.moco.truckparkserver.persistence.FirestoreRepository;
 import de.htwg.moco.truckparkserver.service.FirestoreService;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@CrossOrigin
 public class Controller {
 
     @Resource
@@ -15,6 +26,9 @@ public class Controller {
 
     @Resource
     FirestoreRepository firestoreRepository;
+
+    @Resource
+    ObjectMapper objectMapper;
 
     @GetMapping("/online")
     public String online() {
@@ -38,5 +52,18 @@ public class Controller {
 
         parkingLotHtwgKonstanz.setGeofencePosition(new com.google.maps.model.LatLng(47.668110, 9.169001));
         firestoreRepository.addHtwgParkingLotForTesting(parkingLotHtwgKonstanz);
+    }
+
+    @PostMapping("/parkinglots")
+    public ResponseEntity getParkingLotsOnRoute(@RequestBody String s) throws IOException {
+        System.out.println(s);
+        JSONObject jsonObject = new JSONObject(s);
+        List<LatLng> path = new ArrayList<>();
+        for(int i = 0; i < jsonObject.length(); i++){
+            System.out.println(jsonObject.toString());
+            path.add(objectMapper.readValue(jsonObject.getString(String.valueOf(i)),LatLng.class));
+        }
+        System.out.println(jsonObject);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
