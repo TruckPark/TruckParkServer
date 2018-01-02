b174acf8f3e93dc1c11d5c7121675a4294c47f5d
Lukas Hansen <luhansen@htwg-konstanz.de>
2018-01-02 16:50:25 +0100

    firebase env vars

--- src/main/java/de/htwg/moco/truckparkserver/config/FirestoreConfig.java
+++ src/main/java/de/htwg/moco/truckparkserver/config/FirestoreConfig.java
@@ -1,19 +1,21 @@
 package de.htwg.moco.truckparkserver.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.database.FirebaseDatabase;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.annotation.Resource;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -23,13 +25,58 @@ public class FirestoreConfig {
         return FirestoreClient.getFirestore();
     }
 
+    @Resource
+    private ObjectMapper objectMapper;
+
     @PostConstruct
     public void firebaseService() throws IOException {
-        InputStream serviceAccount = new FileInputStream("src\\main\\resources\\TruckParkMoco-31609a1b4551.json");
-        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(credentials)
-                .build();
-        FirebaseApp.initializeApp(options);
+        String path = "src\\main\\resources\\TruckParkMoco-31609a1b4551.json";
+
+        // if file exists use configuration of file, else use env vars
+        if (new File(path).exists()) {
+            InputStream serviceAccount = new FileInputStream(path);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(credentials)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } else {
+            FirebaseConfig firebaseConfig = new FirebaseConfig(
+                    System.getenv("firebase.config.type"),
+                    System.getenv("firebase.config.project_id"),
+                    System.getenv("firebase.config.private_key_id"),
+                    System.getenv("firebase.config.private_key"),
+                    System.getenv("firebase.config.client_email"),
+                    System.getenv("firebase.config.client_id"),
+                    System.getenv("firebase.config.auth_uri"),
+                    System.getenv("firebase.config.token_uri"),
+                    System.getenv("firebase.config.auth_provider_x509_cert_url"),
+                    System.getenv("firebase.config.client_x509_cert_url"),
+                    null
+            );
+
+            byte[] firebaseConfigBytes = objectMapper.writeValueAsBytes(firebaseConfig);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfigBytes));
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(credentials)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        }
+    }
+
+    @AllArgsConstructor
+    class FirebaseConfig {
+        public String type;
+        public String projectId;
+        public String privateKeyId;
+        public String privateKey;
+        public String clientEmail;
+        public String clientId;
+        public String authUri;
+        public String tokenUri;
+        public String authProviderX509CertUrl;
+        public String clientX509CertUrl;
+        private Map<String, Object> additionalProperties = new HashMap<String, Object>();
     }
 }
